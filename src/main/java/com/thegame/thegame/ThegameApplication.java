2d57de9ce4dfd0f0f399ab524ad40f6d954f5603
stelios <bpstelios10@hotmail.com>
2018-09-04 23:54:37 +0300

    [BUILD] Initialization of spring boot

---
+++ src/main/java/com/thegame/thegame/ThegameApplication.java
@@ -0,0 +1,12 @@
+package com.thegame.thegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThegameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThegameApplication.class, args);
+	}
+}
---
+++ src/test/java/com/thegame/thegame/ThegameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.thegame.thegame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThegameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
