2cab84ea8decafaf313052f9ce3872f1db9826b7
stelios <bpstelios10@hotmail.com>
2018-09-05 01:06:07 +0300

    [users] Creates user dao

---
+++ src/main/java/com/thegame/thegame/model/User.java
@@ -0,0 +1,65 @@
+package com.thegame.thegame.model;
+
+import java.sql.Date;
+
+public class User {
+    
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private char[] credential;
+    private Date createdDate;
+    private Date updatedDate;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public char[] getCredential() {
+        return credential;
+    }
+    public void setCredential(char[] credential) {
+        this.credential = credential;
+    }
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+}
